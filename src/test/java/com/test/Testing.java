238cb68908e3f38712174f218dffa34d23e6213e
viveksha16 <viveks161295@gmail.com>
2022-11-07 18:21:29 +0530

    msg

---
+++ src/test/java/com/test/Testing.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Testing {
+private void method1() {
+	// TODO Auto-generated method stub
+
+}
+private void method2() {
+	// TODO Auto-generated method stub
+
+}
+private void method3() {
+	// TODO Auto-generated method stub
+
+}
+}
