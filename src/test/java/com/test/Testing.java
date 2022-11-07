38a5fb1a740cbb9f8d6bc67acab29f0b956e3fa1
viveksha16 <viveks161295@gmail.com>
2022-11-07 18:44:07 +0530

    msg

--- src/test/java/com/test/Testing.java
+++ src/test/java/com/test/Testing.java
@@ -13,4 +13,16 @@ private void method3() {
 	// TODO Auto-generated method stub
 
 }
+private void method4() {
+	// TODO Auto-generated method stub
+	
+}
+private void method5() {
+	// TODO Auto-generated method stub
+	
+}
+private void method6() {
+	// TODO Auto-generated method stub
+	
+}
 }
